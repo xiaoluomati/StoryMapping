43316fd4b147756241a313be455112260ea99ae7
awayz <2464226202@qq.com>
2019-01-03 19:50:06 +0800

    create project

---
+++ src/main/java/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import po.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+// for test
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
