deea9023bc29c6563feda68dc6105923278b173d
Denny-hu <857672790@qq.com>
2019-01-10 14:21:58 +0800

    delete greeting

--- src/main/java/cn/nju/edu/entity/User.java
+++ src/main/java/cn/nju/edu/entity/User.java
@@ -20,6 +20,11 @@ public class User {
         this.name = name;
         this.password = password;
     }
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
     public int getId() {
         return id;
     }
