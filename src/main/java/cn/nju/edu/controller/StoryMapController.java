7880e6dd2d508ae484fd7bbb735ec7f627c3b0ae
xhw <xhw@xhwdeMacBook-Pro.local>
2019-01-19 19:59:03 +0800

    add excel controller

--- src/main/java/cn/nju/edu/controller/StoryMapController.java
+++ src/main/java/cn/nju/edu/controller/StoryMapController.java
@@ -2,9 +2,20 @@ package cn.nju.edu.controller;
 
 import cn.nju.edu.service.StoryMapService;
 import cn.nju.edu.vo.StoryMapVo;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 
@@ -35,8 +46,45 @@ public class StoryMapController {
         storyMapService.deleteStoryMap(storyMapVo);
     }
 
-    @GetMapping("/exportExcel")
-    public void exportExcel(@RequestParam(value="storyId", defaultValue="1") int storyId){
+//    @GetMapping("/exportExcel")
+//    public void exportExcel(@RequestParam(value="storyId", defaultValue="1") int storyId){
+//        storyMapService.exportExcel(storyId);
+//    }
+
+    @GetMapping(value = "/exportExcel")
+    public ResponseEntity<Resource> exportExcel(@RequestParam(value="storyId", defaultValue="1") int storyId){
         storyMapService.exportExcel(storyId);
+        ByteArrayOutputStream bos = null;
+        String filename = "src/main/resources/Excel/storyMap.xlsx";
+        try {
+            Workbook workbook = new XSSFWorkbook();
+            bos = new ByteArrayOutputStream();
+            workbook.write(bos);
+            workbook.close();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("charset", "utf-8");
+            //设置下载文件名
+            filename = URLEncoder.encode(filename, "UTF-8");
+            headers.add("Content-Disposition", "attachment;filename=\"" + filename + "\"");
+
+            Resource resource = new InputStreamResource(new ByteArrayInputStream(bos.toByteArray()));
+
+            return ResponseEntity.ok().headers(headers).contentType(MediaType.parseMediaType("application/x-msdownload")).body(resource);
+
+        } catch (IOException e) {
+            if (null != bos) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
+
